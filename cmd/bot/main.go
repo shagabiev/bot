fec63616d2c8a01b3630bf04e82f97ea522c6c48
Amir Shagabiev <amir.shagabiev@inbox.ru>
2025-09-16 14:15:57 +0300

    Add /help command

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
 )
 
-const token = "7974384870:AAF7MjPXfwX6QrHAJEaMp5HTfo3xZ0APvk8"
+//const token = "7974384870:AAF7MjPXfwX6QrHAJEaMp5HTfo3xZ0APvk8"
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	token := os.Getenv("TOKEN")
+	if token == "" {
+		log.Panic("Token not found")
+	}
+
+	fmt.Println("Loaded token: ", token)
+
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Panic(err)
@@ -28,13 +42,30 @@ func main() {
 	}
 
 	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		if update.Message == nil {
+			continue
+		}
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: "+update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
+		switch update.Message.Command() {
+		case "help":
+			helpCommand(bot, update.Message)
+		default:
+			defaultBehavior(bot, update.Message)
 		}
 	}
+}
+
+func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "/help - help")
+
+	bot.Send(msg)
+}
+
+func defaultBehavior(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
+
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "You wrote: "+inputMessage.Text)
+	msg.ReplyToMessageID = inputMessage.MessageID
+
+	bot.Send(msg)
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/shagabiev/bot
 go 1.22.1
 
 require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
